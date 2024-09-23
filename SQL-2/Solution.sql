2a96630d528c9aebd3e98294301ee3303f3a30b0
Maheshwaran G <139232419+MaheshwaranG16@users.noreply.github.com>
2024-09-23 11:14:54 +0530

    Files added

--- SQL-2/Solution.sql
+++ SQL-2/Solution.sql
@@ -1,22 +1,22 @@
 WITH trails1 AS
 	(SELECT t1.hut1 start_hut, 
-			h1.name start_hut_name, 
-            h1.altitude start_altitude,
-            t1.hut2 end_hut
+		h1.name start_hut_name, 
+            	h1.altitude start_altitude,
+            	t1.hut2 end_hut
 	FROM mountain_huts h1 JOIN trails t1 
-    ON h1.id =t1.hut1),
+    	ON h1.id =t1.hut1),
 
 trails2 as
     (SELECT t2.*, 
-			h2.name end_hut_name, 
+	    h2.name end_hut_name, 
             h2.altitude end_altitude
     FROM trails1 t2 JOIN mountain_huts h2 
     ON h2.id = t2.end_hut),
 
 trails3 as    
     (SELECT trails2.*,
-			CASE WHEN start_altitude > end_altitude 
-				 THEN 1 ELSE 0 END AS af
+	    CASE WHEN start_altitude > end_altitude 
+		 THEN 1 ELSE 0 END AS af
      FROM trails2),
      
 Final_cte as
@@ -30,9 +30,9 @@ Final_cte as
      FROM trails3)
     
 SELECT 
-	F1.Start_hut_name start_pt,
+    F1.Start_hut_name start_pt,
     F1.end_hut_name as mid_pt, 
     F2.end_hut_name end_pt  
 FROM Final_cte F1 JOIN Final_cte F2 
 ON F1.end_hut = F2.start_hut
-ORDER BY start_pt
+ORDER BY start_pt;

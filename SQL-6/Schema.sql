93bd71dc81b5463cfbef8e8a157748d64027aa7f
Maheshwaran G <maheshwarang16@gmail.com>
2024-09-27 14:43:52 +0530

    Files added

---
+++ SQL-6/Schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Q4DATA
+	(id int,
+    name varchar(20),
+    location varchar(20),
+    PRIMARY KEY(id));
+    
+INSERT INTO Q4DATA(id,name,location)
+	VALUE
+		(1,null,null),
+        (2,"David",null),
+        (3,null,"London"),
+        (4,null,null),
+        (5,"David",null);
---
+++ SQL-6/Solution.sql
@@ -0,0 +1,2 @@
+SELECT max(id) id,max(name) name,max(location) location
+FROM Q4DATA;
